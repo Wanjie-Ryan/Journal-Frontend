fe94eaf1ec48f0bb5d2a1921be48e81d29d160af
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-05 17:49:38 +0300

    create single journal page design

--- src/components/dashboard/singleJournal.js
+++ src/components/dashboard/singleJournal.js
@@ -56,8 +56,11 @@ const JournalDetail = ({ route, navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:30,
+    marginTop: 30,
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
     padding: 20,
   },
   title: {
