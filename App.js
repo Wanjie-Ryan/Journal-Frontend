32cc609ba2a13a82c92d7e203bd3bea356d0dbfc
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-08 08:53:12 +0300

    API Integration

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import AppNavigator from "./src/navigation/AppNavigator";
-
+import { RegContextProvider } from "./src/context/RegContext";
 const App = () => {
   return (
-    <PaperProvider>
-      <AppNavigator />
-    </PaperProvider>
+    <RegContextProvider>
+      <PaperProvider>
+        <AppNavigator />
+      </PaperProvider>
+    </RegContextProvider>
   );
 };
 
--- src/components/Auth/register.js
+++ src/components/Auth/register.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import { View, StyleSheet } from "react-native";
 import { TextInput, Button, Text } from "react-native-paper";
-
+import { RegContext } from "../../context/RegContext";
 const Register = ({ navigation }) => {
+
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsername = (e)=>{
+    setUsername(e.target.value)
+  }
+
+  const handleEmail = (e)=>{
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e)=>{
+    setPassword(e.target.value)
+  }
+
+  const handleRegister = (e)=>{
+
+
+  }
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome To Your Journal Application </Text>
-      <TextInput label="Username" mode="outlined" style={styles.input} />
-      <TextInput label="Email" mode="outlined" style={styles.input} />
+      <TextInput label="Username" mode="outlined" style={styles.input} value={username} onChangeText={setUsername} />
+      <TextInput label="Email" mode="outlined" style={styles.input} value={email} onChangeText={setEmail}/>
       <TextInput
         label="Password"
         mode="outlined"
         secureTextEntry
         style={styles.input}
+        value={password}
+        onChangeText={setPassword}
       />
-      <Button mode="contained" onPress={() => navigation.navigate("Login")}>
+      <Button mode="contained" onPress={handleRegister}>
         Register
       </Button>
       <Text style={styles.link} onPress={() => navigation.navigate("Login")}>
