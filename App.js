32848e73723e95e3772247ad80487a147166a918
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-08 10:36:38 +0300

    Login API Integration

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { RegContextProvider } from "./src/context/RegContext";
+import { LogContextProvider } from "./src/context/LogContext";
 const App = () => {
   return (
     <RegContextProvider>
-      <PaperProvider>
-        <AppNavigator />
-      </PaperProvider>
+      <LogContextProvider>
+        <PaperProvider>
+          <AppNavigator />
+        </PaperProvider>
+      </LogContextProvider>
     </RegContextProvider>
   );
 };
